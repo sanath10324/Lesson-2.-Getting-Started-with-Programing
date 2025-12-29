eab401d8b9926adde6220c391b951a14eba4f088
sanath10324 <sanathkatkamgamer@gmail.com>
2025-12-29 13:22:25 +0400

    Birthday Storing Project

---
+++ birtdayvariable.py
@@ -0,0 +1,13 @@
+birthday_1 = (5,8,2008)
+birthday_2 = (7,10,2007)
+birthday_3 = (9,4,2008)
+birthday_4 = (27,6,2007)
+birthday_5 = (16,2,2008)
+
+print("My Freinds Birthday's:\n")
+
+print("Ram's B-Day", birthday_1)
+print("Akhil's B-day", birthday_2)
+print("Shankar's B-day", birthday_3)
+print("Jain's B-day", birthday_4)
+print("Adil's B-day", birthday_5)
